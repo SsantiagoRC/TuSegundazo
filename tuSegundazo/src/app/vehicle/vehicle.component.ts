806b616d04e0e75b020ad22ff371229fdd6517fd
SantiagoRC <ds.rondon@uniandes.edu.co>
2023-04-30 01:17:31 -0500

    Punto 2 finalizado

--- tuSegundazo/src/app/vehicle/vehicle.component.ts
+++ tuSegundazo/src/app/vehicle/vehicle.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Vehicle } from './vehicle';
 import { VehicleService } from './vehicle.service';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-vehicle',
@@ -10,11 +11,20 @@ import { VehicleService } from './vehicle.service';
 export class VehicleComponent implements OnInit {
 
   public vehicles: Array<Vehicle>=[];
+  public brandCounts : {[key: string]: number} = {};
   constructor(private vehicleService:VehicleService) { }
 
   getVehicles(): void {
     this.vehicleService.getVehicles().subscribe((vehicles) => {
       this.vehicles = vehicles;
+
+      for (const vehicle of vehicles) {
+        if (this.brandCounts.hasOwnProperty(vehicle.marca)) {
+          this.brandCounts[vehicle.marca]++;
+        } else {
+          this.brandCounts[vehicle.marca] = 1;
+        }
+      }
     });
   }
 
--- tuSegundazo/src/app/vehicle/vehicle.module.ts
+++ tuSegundazo/src/app/vehicle/vehicle.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VehicleComponent } from './vehicle.component';
+import { KeyValuePipe } from '@angular/common';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [VehicleComponent],
-  exports: [VehicleComponent]
+  exports: [VehicleComponent],
+  providers:[VehicleComponent, KeyValuePipe ]
 })
 export class VehicleModule { }
